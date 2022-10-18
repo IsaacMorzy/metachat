c22d1ee9a7db305e19f5fd7c52924b2acc4481d7
IsaacMorzy <musyokaisaac98@gmail.com>
2022-10-18 15:14:53 +0300

    user avatar with message

--- components/Messages.js
+++ components/Messages.js
@@ -1,10 +1,29 @@
 import { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
 import SendMessage from "./SendMessage";
+import Message from "./Message";
+
+// only show messages from the last 15 minutes
+const MINS_DURATION = 15;
 
 function Messages() {
-    const {user} = useMoralis();
-    const endOfMessagesRef = useRef(null);
+  const { user } = useMoralis();
+  const endOfMessagesRef = useRef(null);
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
+  console.log(data);
   return (
     <div className="pb-56">
       <div className="my-5">
@@ -13,17 +32,19 @@ function Messages() {
           style={{ marginLeft: "auto", marginRight: "auto" }}
         />
       </div>
-      <div></div>
-      {/* each message */}
+      <div className="space-y-10 p-4">
+        {data.map(message => (
+          <Message key={message.id} message={message}/>
+        ))}
+      </div>
+    
       <div className="flex justify-center">
-         <SendMessage endOfMessagesRef={endOfMessagesRef} />
+        <SendMessage endOfMessagesRef={endOfMessagesRef} />
       </div>
 
-     <div 
-     ref={endOfMessagesRef}
-     className="text-center text-gray-400 mt-5">
+      <div ref={endOfMessagesRef} className="text-center text-gray-400 mt-5">
         <p> You are up to date {user.getUsername()} !</p>
-     </div>
+      </div>
     </div>
   );
 }
